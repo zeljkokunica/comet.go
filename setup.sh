f73a34ede25277659da658082a1bce662dc2ca6c
Zeljko Kunica <zeljko.kunica@gmail.com>
2013-08-13 16:42:21 +0200

    setup and build script

--- setup.sh
+++
@@ -1,5 +0,0 @@
-export GOPATH=$PWD
-go get code.google.com/p/go.net/websocket
-go build github.com/zeljokunica/comet_server
-mkdir $GOPATH/bin/data
-cp $GOPATH/web $GOPATH/bin/
---
+++ setup_and_build.sh
@@ -0,0 +1,3 @@
+export GOPATH=$PWD
+go get code.google.com/p/go.net/websocket
+go build github.com/zeljkokunica/comet_server
